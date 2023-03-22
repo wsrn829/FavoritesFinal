d353214a8d9fd6c4be215d67b58ed33e5fdb6070
wsrn829 <wsrn829@gmail.com>
2023-03-21 22:01:35 -0700

    my-version

--- questions.py
+++ questions.py
@@ -1,17 +1,17 @@
 # Welcome to the final Git exercise.
 # We're in a Python file again, but it looks like
-# we've been beaten to the punch! Please 
+# we've been beaten to the punch! Please
 # follow the instructions in Learn to get the repository
 # into a state where you can fill in your own answers!
 
 """
-!!!!! 
+!!!!!
   DO NOT MODIFY ANY OF THE CODE BELOW THIS LINE
 !!!!!
 """
 
 first_question = "What is your favorite color?"
-first_question_answer = ""
+first_question_answer = "purple"
 
 second_question = "What is your favorite food?"
 second_question_answer = ""
