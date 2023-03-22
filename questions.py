d048db99e983f0f9adb85c261262d4fe2f0a2e02
wsrn829 <wsrn829@gmail.com>
2023-03-21 22:20:29 -0700

    third-question

--- questions.py
+++ questions.py
@@ -17,7 +17,7 @@ second_question = "What is your favorite food?"
 second_question_answer = "spicy food"
 
 third_question = "Who is your favorite fictional character? ex. Mickey Mouse, Bugs Bunny"
-third_question_answer = ""
+third_question_answer = "Dr Who"
 
 fourth_question = "What is your favorite animal?"
 fourth_question_answer = ""
